d1b168a5825014fd10ce8c61b874cae5d10986a5
William Villanueva <wvillanu89@gmail.com>
2018-03-09 17:15:30 -0600

    bounty stage is now human readable

--- bounties_api/std_bounties/serializers.py
+++ bounties_api/std_bounties/serializers.py
@@ -3,6 +3,15 @@ from std_bounties.models import Bounty, Fulfillment, Category, RankedCategory, T
 from std_bounties.constants import STAGE_CHOICES
 
 
+class ChoicesField(serializers.Field):
+    def __init__(self, choices, **kwargs):
+        self._choices = choices
+        super(ChoicesField, self).__init__(**kwargs)
+
+    def to_representation(self, obj):
+        return self._choices[obj][1]
+
+
 class RankedCategorySerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -29,9 +38,8 @@ class TokenSerializer(serializers.ModelSerializer):
         model = Token
         fields = '__all__'
 
-
 class BountySerializer(serializers.ModelSerializer):
-    bountyStage = serializers.CharField(source='get_bountyStage_display')
+    bountyStage = ChoicesField(choices=STAGE_CHOICES)
     categories = CategorySerializer(read_only=True, many=True)
     current_market_token_data = TokenSerializer(read_only=True, source='token')
     fulfillment_count = serializers.ReadOnlyField(source='fulfillment_set.count')

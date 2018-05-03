4b4cba924db299d623667b0242fc27d968ab375b
William Villanueva <wvillanu89@gmail.com>
2018-05-03 17:30:53 -0500

    Include notification_id field - this represents the notificaiton identifier - which ultimately gives the appropriate action.

--- bounties_api/notifications/models.py
+++ bounties_api/notifications/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Notification(models.Model):
     user = models.ForeignKey('authentication.User', null=False)
     created = models.DateTimeField(auto_now_add=True)
+    notification_id = models.IntegerField(null=False)
     notification_created = models.DateTimeField(null=False)
     email = models.BooleanField(default=False, null=False)
     dashboard = models.BooleanField(default=True, null=False)

103f6cc1b3d1a587848c005209e2b3117e23dd12
William Villanueva <wvillanu89@gmail.com>
2018-05-03 18:47:35 -0500

    Further templates and notification events were added. Need confirmation and discussion on what should be there as the root/base.

--- bounties_api/notifications/notification_client.py
+++ bounties_api/notifications/notification_client.py
@@ -1,7 +1,8 @@
-from std_bounties.models import Fulfillment, Bounty
+from std_bounties.models import Fulfillment, Bounty, BountyState
 from notifications.models import Notification, NotificationDashboard
-from notifications.constants import FULFILLMENT_SUBMITTED
+from notifications.constants import *
 from notifications.notification_templates import *
+from std_bounties.constants import EXPIRED, ACTIVE
 
 class NotificationClient:
     def __init__(self):
@@ -9,12 +10,12 @@ class NotificationClient:
 
     def fulfillment_submitted(self, bounty_id, fulfillment_id):
         bounty = Bounty.objects.get(id=bounty_id)
-        fulfillment = Fulfillment.objects.get(id=fulfillment_id)
-        string_data_fulfiller = FULFILLMENT_SUBMITTED_FULFILLER.format(bounty_title=bounty.title)
-        string_data_issuer = FULFILLMENT_SUBMITTED_ISSUER.format(bounty_title=bounty.title)
+        fulfillment = Fulfillment.objects.get(id=fulfillment_id, bounty=bounty)
+        string_data_fulfiller = FULFILLMENT_SUBMITTED_FULFILLER_STR.format(bounty_title=bounty.title)
+        string_data_issuer = FULFILLMENT_SUBMITTED_ISSUER_STR.format(bounty_title=bounty.title)
         notification_fulfiller = Notification.objects.create(
             notification_id=FULFILLMENT_SUBMITTED,
-            fulfillment.user,
+            user=fulfillment.user,
             notification_created=fulfillment.fulfillment_created,
             email=True,
             dashboard=True
@@ -25,7 +26,7 @@ class NotificationClient:
         )
         notification_issuer = Notification.objects.create(
             notification_id=FULFILLMENT_SUBMITTED,
-            fulfillment.user,
+            user=bounty.user,
             notification_created=fulfillment.fulfillment_created,
             email=True,
             dashboard=True
@@ -36,11 +37,52 @@ class NotificationClient:
         )
         # Once we include email, email client call added here
 
-    def bounty_submitted(self, bounty_id):
-        pass
+    def bounty_activated(self, bounty_id):
+        bounty = Bounty.objects.get(bounty_id)
+        bounty_state = BountyState.objects.filter(bounty=bounty, bountyStage=ACTIVE).latest()
+        string_data = BOUNTY_ACTIVATED_STR.format(bounty_title=bounty.title)
+        notification = Notification.objects.create(
+            notification_id=BOUNTY_ACTIVATED,
+            user=bounty.user,
+            notification_created=bounty_state.change_date,
+            email=True,
+            dashboard=True
+        )
+        NotificationDashboard.objects.create(
+            notification=notification,
+            string_data=string_data
+        )
+
 
     def fulfillment_accepted(self, bounty_id, fulfillment_id):
-        pass
+        bounty = bounty.objects.get(id=bounty_id)
+        fulfillment = fulfillment.objects.get(bounty_id=bounty, fulfillment_id=fulfillment_id)
+        string_data = FULFILLMENT_ACCEPTED_STR.format(bounty_title=bounty_title)
+        notification = Notification.objects.create(
+            notification_id=FULFILLMENT_ACCEPTED,
+            user=bounty.user,
+            notification_created=fulfillment.accepted_date,
+            email=True,
+            dashboard=True
+        )
+        NotificationDashboard.objects.create(
+            notification=notification,
+            string_data=string_data
+        )
+
 
     def bounty_expired(self, bounty_id):
-        pass
+        bounty = bounty.objects.get(id=bounty_id)
+        bounty_state = BountyState.objects.filter(bounty=bounty, bountyStage=ACTIVE).latest()
+        string_data = BOUNTY_EXPIRED_STR.format(bounty_title=bounty_title)
+        notification = Notification.objects.create(
+            notification_id=BOUNTY_EXPIRED,
+            user=bounty.user,
+            notification_created=bounty_state.change_date,
+            email=True,
+            dashboard=True
+        )
+        NotificationDashboard.objects.create(
+            notification=notification,
+            string_data=string_data
+        )
--- bounties_api/notifications/notification_templates.py
+++ bounties_api/notifications/notification_templates.py
@@ -2,6 +2,20 @@ FULFILLMENT_SUBMITTED_FULFILLER = """
 Your Fulfillment Has Been Submitted for {bounty_title}
 """
 
+# Question or Corwin and Mark - do we even want notifications around this?
 FULFILLMENT_SUBMITTED_ISSUER = """
 You Have Received a Fulfillment for {bounty_title}
+"""
+
+BOUNTY_ACTIVATED = """
+Your Bounty Has Been Released to the Wild: {bounty_title}
+"""
+
+# Do we need one for the issuer?
+FULFILLMENT_ACCEPTED = """
+Your fulfillment has been accepted for bounty {bounty_title}
+"""
+
+BOUNTY_EXPIRED = """
+Yikes! Your Bounty has Expired for bounty {bounty_title}
 """

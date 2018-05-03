c4ac9f2494cb87a53e4f97ae1c5d27c83cde8f40
William Villanueva <wvillanu89@gmail.com>
2018-05-03 17:21:12 -0500

    For now include email and dashboard field. This will likely change in the future.

--- bounties_api/notifications/models.py
+++ bounties_api/notifications/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Notification(models.Model):
+    user = models.ForeignKey('authentication.User', null=False)
     created = models.DateTimeField(auto_now_add=True)
-    notification_created = models.DateTimeField(null=True)
-    user = models.ForeignKey('authentication.User')
-    viewed = models.BooleanField()
+    notification_created = models.DateTimeField(null=False)
+    email = models.BooleanField(default=False, null=False)
+    dashboard = models.BooleanField(default=True, null=False)
+    subscribed = models.BooleanField(default=True, null=False)
+    viewed = models.BooleanField(default=False, null=False)

56f0590ad741c115d89cf89bec4ec0ad5a90a866
William Villanueva <wvillanu89@gmail.com>
2018-05-03 18:03:51 -0500

    Include the user creation logic

--- bounties_api/authentication/management/commands/script_create_initial_users.py
+++ bounties_api/authentication/management/commands/script_create_initial_users.py
@@ -8,16 +8,32 @@ logger = logging.getLogger('django')
 
 
 class Command(BaseCommand):
+    # This is a temp script/migration script - not something that runs regularly
     help = 'Script to Create the Users From Current Bounties and Fulfillments'
 
     def handle(self, *args, **options):
         try:
             bounties = Bounty.objects.all()
             for bounty in bounties:
-                
+                name = bounty.issuer_name
+                email = bounty.issuer_email
+                public_address = bounty.issuer_address
+                User.objects.create(
+                    name=name,
+                    email=email,
+                    public_address=public_address
+                )
             fulfillments = Fulfillment.objects.all()
-            for fulfillment in fulfillments:
 
+            for fulfillment in fulfillments:
+                name = fulfillment.fulfiller_name
+                email = fulfillment.fulfiller_email
+                public_address = fulfillment.fulfiller_address
+                User.objects.create(
+                    name=name,
+                    email=email,
+                    public_address=public_address
+                )
         except Exception as e:
             # goes to rollbar
             logger.exception(e)

8fb399e44df6b8e5f66be8083f1c88c51f7b9d37
William Villanueva <wvillanu89@gmail.com>
2018-05-03 17:29:59 -0500

    Add bounties notficiations sqs url

--- bounties_api/bounties/settings.py
+++ bounties_api/bounties/settings.py
@@ -191,6 +191,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_URL = 'https://s3.amazonaws.com/assets.bounties.network/' + ENVIRONMENT + '/'
 QUEUE_URL = os.environ.get('queue_url', 'https://sqs.us-east-1.amazonaws.com/802922962628/bounties_development.fifo')
+NOTIFICATIONS_URL = os.environ.get('notifications_url', 'https://sqs.us-east-1.amazonaws.com/802922962628/notifications_development.fifo')
 SLACK_TOKEN = os.environ.get('slack_token')
 REDIS_LOCATION = os.environ.get('redis_location', 'redis://127.0.0.1:6379')
 LOCAL = os.environ.get('local') == 'true'

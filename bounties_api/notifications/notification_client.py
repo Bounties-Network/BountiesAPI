2e676a278e3dcbd0a6cf578f59adb73a19d50ecb
William Villanueva <wvillanu89@gmail.com>
2018-05-03 18:22:22 -0500

    Included notification template strings, and added them to the notification client.

--- bounties_api/notifications/notification_client.py
+++ bounties_api/notifications/notification_client.py
@@ -1,6 +1,7 @@
 from std_bounties.models import Fulfillment, Bounty
 from notifications.models import Notification, NotificationDashboard
 from notifications.constants import FULFILLMENT_SUBMITTED
+from notifications.notification_templates import *
 
 class NotificationClient:
     def __init__(self):
@@ -9,16 +10,29 @@ class NotificationClient:
     def fulfillment_submitted(self, bounty_id, fulfillment_id, timestamp):
         bounty = Bounty.objects.get(id=bounty_id)
         fulfillment = Fulfillment.objects.get(id=fulfillment_id)
-        string_data = 'Fulfillment Submitted for: {}'.format(bounty.title)
-        notification = Notification.objects.create(
+        string_data_fulfiller = FULFILLMENT_SUBMITTED_FULFILLER.format(bounty_title=bounty.title)
+        string_data_issuer = FULFILLMENT_SUBMITTED_ISSUER.format(bounty_title=bounty.title)
+        notification_fulfiller = Notification.objects.create(
             notification_id=FULFILLMENT_SUBMITTED,
-            # user=TODO,
+            fulfillment.user,
             notification_created=timestamp,
             email=True,
             dashboard=True
         )
         NotificationDashboard.objects.create(
-            notification=notification,
-            string_data=string_data
+            notification=notification_fulfiller,
+            string_data=string_data_fulfiller
         )
+        notification_issuer = Notification.objects.create(
+            notification_id=FULFILLMENT_SUBMITTED,
+            fulfillment.user,
+            notification_created=timestamp,
+            email=True,
+            dashboard=True
+        )
+        NotificationDashboard.objects.create(
+            notification=notification_issuer,
+            string_data=string_data_issuer
+        )
+
         # Once we include email, email client call added here

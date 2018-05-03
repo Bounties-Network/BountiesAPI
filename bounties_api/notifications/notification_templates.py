b271fcdd206d14d03e0bcb38b8390561480f529c
William Villanueva <wvillanu89@gmail.com>
2018-05-03 18:22:22 -0500

    Included notification template strings, and added them to the notification client.

---
+++ bounties_api/notifications/notification_templates.py
@@ -0,0 +1,7 @@
+FULFILLMENT_SUBMITTED_FULFILLER = """
+Your Fulfillment Has Been Submitted for {bounty_title}
+"""
+
+FULFILLMENT_SUBMITTED_ISSUER = """
+You Have Received a Fulfillment for {bounty_title}
+"""

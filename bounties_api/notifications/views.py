18f2a5465b86b19a8cc3bfce0f510982ab7c6c0f
William Villanueva <wvillanu89@gmail.com>
2018-05-03 19:16:11 -0500

    Included the notification viewed endpoint

--- bounties_api/notifications/views.py
+++ bounties_api/notifications/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from notifications.models import DashboardNotification
 from notifications.serializers import DashboardNotificationSerializer
 from notifications.filters import DashboardNotificationFilter
@@ -9,3 +10,10 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DashboardNotification.objects.all().select_related('notification')
     filter_class = DashboardNotificationFilter
     filter_backends = (DjangoFilterBackend,)
+
+class NotificationViewed(APIView):
+    # This view needs to only be accessible if the user is logged in
+    def get(self, request, notification_id):
+        notification = getOr404(DashboardNotification, id=notification_id)
+        notification.viewed = True
+        notification.save()

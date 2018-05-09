cd2369f34570bcab9e5d57a253462ddc024fe92e
William Villanueva <wvillanu89@gmail.com>
2018-05-09 16:14:47 -0400

    Add SQL style string utils

--- bounties_api/bounties/utils.py
+++ bounties_api/bounties/utils.py
@@ -8,6 +8,31 @@ max_datetime = datetime.datetime(9999, 12, 31, 23, 59, 59, 999999)
 max_time_stamp = time.mktime(max_datetime.timetuple())
 
 
+def sqlGenerateOrList(param_name, count, operation):
+    index = 0
+    sql_string = ''
+    while index < count:
+        sql_string += '{} {} %s'.format(param_name, operation)
+        if index != count - 1:
+            sql_string += ' OR '
+        index += 1
+    return sql_string
+
+
+def extractInParams(request, equals_param, in_param):
+    included_values = []
+    equals = request.GET.get(equals_param, None)
+    includes_raw = request.GET.get(in_param, None)
+    if equals:
+        included_values.append(equals)
+    if includes_raw:
+        includes = includes_raw.split(',')
+        included_values = included_values + includes
+    if len(included_values) == 0:
+        return None
+    return included_values
+
+
 def getDateTimeFromTimestamp(timestamp):
     try:
         integer_stamp = int(timestamp)

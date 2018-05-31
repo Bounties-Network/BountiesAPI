a8558bc7252dd1798a326294d68645b52d06d049
William Villanueva <wvillanu89@gmail.com>
2018-05-31 17:16:26 +0200

    Add User Object permissions.

--- bounties_api/authentication/permissions.py
+++ bounties_api/authentication/permissions.py
@@ -6,6 +6,7 @@ class AuthenticationPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.is_logged_in
 
+
 class UserIDMatches(permissions.BasePermission):
     message = 'Unauthorized'
 
@@ -16,3 +17,10 @@ class UserIDMatches(permissions.BasePermission):
             user_id = request.POST.get('user_id', None)
 
         return bool(user_id)
+
+
+class UserObjectPermissions(permissions.BasePermission):
+    message = 'Unauthorized'
+
+    def has_object_permissions(self, request, view, obj):
+        return obj.user == request.current_user

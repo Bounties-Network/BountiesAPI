74dae291dc62416f02ae765bc92f39d688731abd
William Villanueva <wvillanu89@gmail.com>
2018-05-31 16:32:37 +0200

    Permissions wrappers added.

---
+++ bounties_api/authentication/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+class AuthenticationPermission(permissions.BasePermission):
+    message = 'Unauthorized'
+
+    def has_permission(self, request, view):
+        return request.is_logged_in
+
+class UserIDMatches(permissions.BasePermission):
+    message = 'Unauthorized'
+
+    def has_permission(self, request, view):
+        if request.GET:
+            user_id = request.GET.get('user_id', None)
+        if request.POST:
+            user_id = request.POST.get('user_id', None)
+
+        return bool(user_id)

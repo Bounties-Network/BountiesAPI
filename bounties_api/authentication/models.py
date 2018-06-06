6b58dfc5b333456c92e0afc731c8ededfdb1f191
William Villanueva <wvillanu89@gmail.com>
2018-06-06 12:07:45 +0200

    Included all user endpoints to update and create. Added many to many category relationship, slug mixin, more permissions mixins.

--- bounties_api/authentication/models.py
+++ bounties_api/authentication/models.py
@@ -1,10 +1,11 @@
 import uuid
+from django.apps import apps
 from django.db import models
 
 class User(models.Model):
     public_address = models.TextField(max_length=500, blank=True)
     nonce = models.UUIDField(default=uuid.uuid4, null=False, blank=False)
-    category = models.ManyToManyField('std_bounties.Category', null=True)
+    categories = models.ManyToManyField('std_bounties.Category', null=True)
     name = models.CharField(max_length=128, blank=True)
     email = models.CharField(max_length=128, blank=True)
     organization = models.CharField(max_length=128, blank=True)

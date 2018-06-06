4c849b206ed2d91c94668db416ee39246caabf15
William Villanueva <wvillanu89@gmail.com>
2018-06-06 12:07:45 +0200

    Included all user endpoints to update and create. Added many to many category relationship, slug mixin, more permissions mixins.

--- bounties_api/authentication/urls.py
+++ bounties_api/authentication/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from authentication import views
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
+
+router = DefaultRouter()
+router.register(r'user', views.UserAddressView)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^login/$', views.Login.as_view()),
     url(r'^logout/$', views.Logout.as_view()),
     url(r'^user/$', views.UserView.as_view()),
---
+++ bounties_api/bounties/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class CreatableSlugRelatedField(serializers.SlugRelatedField):
+
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', slug_name=self.slug_field, value=data)
+        except (TypeError, ValueError):
+            self.fail('invalid')

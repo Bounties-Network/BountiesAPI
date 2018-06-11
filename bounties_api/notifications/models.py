0924902e6706e3104e48b7f98814474d015d732b
William Villanueva <wvillanu89@gmail.com>
2018-06-11 20:30:00 +0200

    Include uid field on notification and email_sent field.

--- bounties_api/notifications/models.py
+++ bounties_api/notifications/models.py
@@ -5,11 +5,12 @@ from notifications.constants import NOTIFICATION_IDS
 
 class Notification(models.Model):
     user = models.ForeignKey('authentication.User', null=False)
+    uid = models.CharField(null=False, blank=False, max_length=512)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     notification_name = models.IntegerField(choices=NOTIFICATION_IDS, null=False)
     notification_created = models.DateTimeField(null=False)
-    email = models.BooleanField(default=False, null=False)
+    email_sent = models.BooleanField(default=False, null=False)
     dashboard = models.BooleanField(default=True, null=False)
 
 

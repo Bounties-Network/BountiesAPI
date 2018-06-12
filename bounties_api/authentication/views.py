d4e2274a3d4459e902cb8a30f95a8a02ff99fef8
William Villanueva <wvillanu89@gmail.com>
2018-06-12 23:16:15 +0200

    Include signup endpoint

--- bounties_api/authentication/views.py
+++ bounties_api/authentication/views.py
@@ -22,6 +22,21 @@ class Login(APIView):
         return JsonResponse(UserSerializer(user).data)
 
 
+class SignUp(APIView):
+    def post(self, request):
+        public_address = request.data.get('public_address', '')
+        signature = request.data.get('signature', '')
+        email = request.data.get('email', None)
+        name = request.data.get('name', None)
+        if not email or not name:
+            return HttpResponse('email and name required', status=400)
+        user = authenticate(public_address=public_address, signature=signature)
+        if not user:
+            return HttpResponse('Unauthorized', status=401)
+        login(request, user)
+        return JsonResponse(UserSerializer(user).data)
+
+
 class Logout(APIView):
     def get(self, request):
         logout(request)
@@ -31,7 +46,7 @@ class Logout(APIView):
 class Nonce(APIView):
     def get(self, request, address=''):
         user = User.objects.get_or_create(public_address=address.lower())[0]
-        return JsonResponse({'nonce': user.nonce, 'has_signed_up': bool(user.email)})
+        return JsonResponse({'nonce': user.nonce, 'has_signed_up': bool(user.email) and bool(user.name)})
 
 
 class UserView(APIView):

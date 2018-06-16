37dc8953f1c5bb4fe9bf3b35347a7a4cc42c12e9
William Villanueva <wvillanu89@gmail.com>
2018-06-16 13:51:37 +0200

    Set up proper validation on the settings endpoint. Remove stray router url for auth (remnant from when we had a user post url)

--- bounties_api/authentication/urls.py
+++ bounties_api/authentication/urls.py
@@ -4,13 +4,10 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 
-router = DefaultRouter()
-
 urlpatterns = [
     url(r'^user/$', views.UserView.as_view()),
     url(r'^user/(?P<public_address>\w+)/nonce/$', views.Nonce.as_view()),
     url(r'^user/(?P<public_address>\w+)/profile/$', views.UserProfile.as_view()),
-    url(r'^', include(router.urls)),
     url(r'^login/$', views.Login.as_view()),
     url(r'^logout/$', views.Logout.as_view()),
     url(r'^settings/$', views.SettingsView.as_view()),

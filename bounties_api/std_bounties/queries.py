0416039e4e47ae9cf09967886bef68dcdba31aae
William Villanueva <wvillanu89@gmail.com>
2018-06-16 18:18:14 +0200

    Update leaderboard endpoints to return the profile_image url that points to our CDN/cloudfront.

--- bounties_api/std_bounties/queries.py
+++ bounties_api/std_bounties/queries.py
@@ -3,9 +3,10 @@
 LEADERBOARD_FULFILLER_QUERY = """
 SELECT
 	fulfillment.fulfiller as address,
-	fulfillment_profiles.fulfiller_name as name,
-	fulfillment_profiles.fulfiller_email as email,
-	fulfillment_profiles."fulfiller_githubUsername" as githubUsername,
+	profile.name as name,
+	profile.email as email,
+	profile.github_username as githubUsername,
+	profile.profile_image as profile_image,
 	SUM(bounty."fulfillmentAmount") as total,
 	SUM(fulfillment.usd_price) as total_usd,
 	COUNT(bounty) as bounties_fulfilled,
@@ -13,53 +14,10 @@ SELECT
 FROM std_bounties_fulfillment fulfillment
 JOIN std_bounties_bounty bounty
 ON fulfillment.bounty_id = bounty.id
-JOIN
-(
-	SELECT
-		fulfillments.fulfiller,
-		fulfiller_name,
-		fulfiller_email,
-		"fulfiller_githubUsername"
-	FROM std_bounties_fulfillment fulfillments
-	INNER JOIN (
-		SELECT
-			fulfiller,
-			MAX(created) as max_date
-		FROM std_bounties_fulfillment
-		WHERE std_bounties_fulfillment.fulfiller_name != ''
-		GROUP BY fulfiller
-		UNION
-		(
-			SELECT
-				without_name.fulfiller,
-				without_name.max_date
-			FROM (
-				SELECT
-					fulfiller,
-					MAX(created) as max_date
-				FROM std_bounties_fulfillment
-				WHERE fulfiller_name != ''
-				GROUP BY fulfiller
-			) with_name
-			RIGHT JOIN
-			(
-				SELECT
-					fulfiller,
-					MAX(created) as max_date
-				FROM std_bounties_fulfillment
-				WHERE fulfiller_name = ''
-				GROUP BY fulfiller
-			) without_name
-			ON with_name.fulfiller = without_name.fulfiller
-			WHERE with_name.fulfiller IS NULL
-		)
-	) max_date_fulfillment
-	ON fulfillments.fulfiller = max_date_fulfillment.fulfiller
-		AND fulfillments.created = max_date_fulfillment.max_date
-) fulfillment_profiles
-ON fulfillment.fulfiller = fulfillment_profiles.fulfiller
+JOIN authentication_user profile
+ON fulfillment.fulfiller = profile.public_address
 WHERE fulfillment.accepted = true {}
-GROUP BY fulfillment.fulfiller, fulfillment_profiles.fulfiller_name, fulfillment_profiles.fulfiller_email, fulfillment_profiles."fulfiller_githubUsername"
+GROUP BY fulfillment.fulfiller, profile.name, profile.email, profile.github_username, profile.profile_image
 ORDER BY total_usd desc, total desc
 """
 
@@ -67,9 +25,10 @@ ORDER BY total_usd desc, total desc
 LEADERBOARD_ISSUER_QUERY = """
 SELECT
 	bounty.issuer as address,
-	bounty_profiles.issuer_name as name,
-	bounty_profiles.issuer_email as email,
-	bounty_profiles."issuer_githubUsername" as githubUsername,
+	profile.name as name,
+	profile.email as email,
+	profile.github_username as githubUsername,
+	profile.profile_image as profile_image,
 	SUM(bounty."fulfillmentAmount") as total,
 	SUM(fulfillment.usd_price) as total_usd,
 	COUNT(distinct(bounty.id)) as bounties_issued,
@@ -77,52 +36,9 @@ SELECT
 FROM std_bounties_fulfillment fulfillment
 JOIN std_bounties_bounty bounty
 ON fulfillment.bounty_id = bounty.id
-JOIN
-(
-	SELECT
-		bounties.issuer,
-		issuer_name,
-		issuer_email,
-		"issuer_githubUsername"
-	FROM std_bounties_bounty bounties
-	INNER JOIN (
-		SELECT
-			issuer,
-			MAX(created) as max_date
-		FROM std_bounties_bounty
-		WHERE issuer_name != ''
-		GROUP BY issuer
-		UNION
-		(
-			SELECT
-				without_name.issuer,
-				without_name.max_date
-			FROM (
-				SELECT
-					issuer,
-					MAX(created) as max_date
-				FROM std_bounties_bounty
-				WHERE issuer_name != ''
-				GROUP BY issuer
-			) with_name
-			RIGHT JOIN
-			(
-				SELECT
-					issuer,
-					MAX(created) as max_date
-				FROM std_bounties_bounty
-				WHERE issuer_name = ''
-				GROUP BY issuer
-			) without_name
-			ON with_name.issuer = without_name.issuer
-			WHERE with_name.issuer IS NULL
-		)
-	) max_date_bounty
-	ON bounties.issuer = max_date_bounty.issuer
-		AND bounties.created = max_date_bounty.max_date
-) bounty_profiles
-ON bounty.issuer = bounty_profiles.issuer
+JOIN authentication_user profile
+ON bounty.issuer = profile.public_address
 WHERE fulfillment.accepted = true {}
-GROUP BY bounty.issuer, bounty_profiles.issuer_name, bounty_profiles.issuer_email, bounty_profiles."issuer_githubUsername"
+GROUP BY bounty.issuer, profile.name, profile.email, profile.github_username, profile.profile_image
 ORDER BY total_usd desc, total desc
 """

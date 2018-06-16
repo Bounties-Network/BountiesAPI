f127791f3532f472e9354fb23dae0d5ee4a1633c
William Villanueva <wvillanu89@gmail.com>
2018-06-16 11:01:44 +0200

    Settings endpoint is now separate. No user endpoint.

--- bounties_api/authentication/urls.py
+++ bounties_api/authentication/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^login/$', views.Login.as_view()),
     url(r'^logout/$', views.Logout.as_view()),
+    url(r'^settings/$', views.SettingsView.as_view()),
 ]

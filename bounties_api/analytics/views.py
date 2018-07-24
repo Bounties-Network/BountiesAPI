82387b7b2bee49830f81e831c82c3268ff4dac0a
Brian Ho <mr.brianhobo@hotmail.com>
2018-07-24 16:35:20 -0700

    update analytics endpoint to support timelines + bounty categories

--- bounties_api/analytics/views.py
+++ bounties_api/analytics/views.py
@@ -4,10 +4,12 @@ import json
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Count
 
 from analytics.filters import BountiesTimelineFilter
-from .serializers import BountiesTimelineSerializer
+from .serializers import BountiesTimelineSerializer, TimelineCategorySerializer
 from .models import BountiesTimeline
+from std_bounties.models import Category, RankedCategory
 
 
 class TimelineBounties(APIView):
@@ -35,7 +37,21 @@ class TimelineBounties(APIView):
                     bounties_timeline.qs, many=True, context={
                         'request': request})
 
-                return Response(serialized.data)
+                ranked_category_list = RankedCategory.objects.distinct().values('normalized_name', 'name')
+                ranked_categories = dict(map(lambda x : (x['normalized_name'],x['name']), ranked_category_list))
+                queryset = Category.objects.select_related('bounty').filter(
+                  bounty__bounty_created__gte=since_date,
+                  bounty__bounty_created__lte=until_date
+                ).distinct().values('normalized_name').annotate(total=Count('bounty'))
+                categories = TimelineCategorySerializer(queryset, many=True, context={'ranked_categories': ranked_categories})
+
+                data = {
+                  'timeline': serialized.data,
+                  'categories': categories.data
+                }
+
+                return Response(data)
+
         except ValueError:
             pass
 

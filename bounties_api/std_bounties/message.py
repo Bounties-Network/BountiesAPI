767e618f1a0efe63b383d9f1d81b80d2327c36f9
Matias Forbord <matias.forbord@gmail.com>
2018-08-10 23:46:43 -0400

    Fix json, date conversion, and tests

--- bounties_api/std_bounties/message.py
+++ bounties_api/std_bounties/message.py
@@ -1,6 +1,17 @@
 import json
 import time
 from datetime import datetime
+from django.core.serializers.json import DjangoJSONEncoder
+
+def to_serializable(val):
+    """JSON serializer for objects not serializable by default"""
+
+    if isinstance(val, datetime):
+        return val.isoformat()
+    elif hasattr(val, '__dict__'):
+        return val.__dict__
+
+    return val
 
 class Message:
     receipt_handle = ''
@@ -11,37 +22,49 @@ class Message:
     transaction_from = ''
     transaction_hash = ''
     event_timestamp = -1
-    event_date = ''
+    event_date = None
     contract_method_inputs = {}
 
-    @classmethod
+    @staticmethod
     def message_from_event(event):
-        if not message:
-            return False
+        if not event:
+            raise ValueError('Can\'t create message without event')
+        elif event.__class__ != dict:
+            raise TypeError('Event argument was not a dict')
 
-        message_attributes = message['MessageAttributes']
+        message_attributes = event['MessageAttributes']
+        event_timestamp = message_attributes['TimeStamp']['StringValue']
 
         return Message(
-            receipt_handle=message['ReceiptHandle'],
+            receipt_handle=event['ReceiptHandle'],
             event=message_attributes['Event']['StringValue'],
             bounty_id=int(message_attributes['BountyId']['StringValue']),
             fulfillment_id=int(message_attributes['FulfillmentId']['StringValue']),
             message_deduplication_id=message_attributes['MessageDeduplicationId']['StringValue'],
             transaction_from=message_attributes['TransactionFrom']['StringValue'],
             transaction_hash=message_attributes['TransactionHash']['StringValue'],
-            event_timestamp=message_attributes['TimeStamp']['StringValue'],
+            event_timestamp=event_timestamp,
             event_date=datetime.fromtimestamp(int(event_timestamp)),
             contract_method_inputs=json.loads(
                 message_attributes['ContractMethodInputs']['StringValue'])
         )
+    
+    @staticmethod
+    def message_from_string(string):
+        if not string:
+            raise ValueError('Can\'t create message without string')
+        elif string.__class__ != str:
+            raise TypeError('Event argument was not a string')
+        
+        dictionary = json.loads(string)
+        dictionary['event_date'] = datetime.strptime(dictionary['event_date'], '%Y-%m-%dT%H:%M:%S')
+        return Message(dictionary)
 
     def __init__(self, *args, **kwargs):
-        if (kwargs):
+        if kwargs:
             self.__dict__.update(kwargs)
-        elif (args):
-            self.__dict__.update(json.loads(*args))
+        elif args:
+            self.__dict__.update(*args)
     
     def __str__(self):
-        modifiedSelf = self.__dict__
-        modifiedSelf.event_date = str(modifiedSelf.event_date)
-        return json.dumps(modifiedSelf)
+        return json.dumps(self.__dict__, indent=4, default=to_serializable)

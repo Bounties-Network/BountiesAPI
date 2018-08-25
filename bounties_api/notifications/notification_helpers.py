830bc733ab0eeabe494c5f572d6d750b674cfac5
Matias Forbord <matias.forbord@gmail.com>
2018-08-24 22:17:49 -0400

    Remove debugging code, pep8 format

--- bounties_api/notifications/notification_helpers.py
+++ bounties_api/notifications/notification_helpers.py
@@ -26,6 +26,8 @@ def create_profile_updated_notification(*args, **kwargs):
 
 @transaction.atomic
 def create_notification(**kwargs):
+    string_data = kwargs['string_data']
+    is_activity = kwargs['is_activity']
 
     notification, created = Notification.objects.get_or_create(
         uid=str(kwargs['uid']),
@@ -39,51 +41,41 @@ def create_notification(**kwargs):
         },
     )
 
-    # this function is atomic, so this is a good way to be sure 
+    # this function is atomic, so this is a good way to be sure
     # we never notify more than once
     if not created:
         return
 
+    DashboardNotification.objects.create(
+        notification=notification,
+        string_data=string_data,
+        is_activity=is_activity,
+        data={'link': url, 'bounty_title': kwargs['bounty'].title},
+    )
 
-    # TODO: Re-enable other notifications
+    email_settings = user.settings.emails
+    activity_emails = email_settings['activity']
 
-    # DashboardNotification.objects.create(
-    #     notification=notification,
-    #     string_data=string_data,
-    #     is_activity=is_activity,
-    #     data={'link': url, 'bounty_title': bounty_title},
-    # )
+    if is_activity and not activity_emails:
+        return
 
-    # email_settings = user.settings.emails
-    # activity_emails = email_settings['activity']
+    if (not is_activity and
+            notification_name not in user.settings.accepted_email_settings()):
+        return
 
-    # if is_activity and not activity_emails:
-    #     return
-
-    # if (not is_activity and
-    #     notification_name not in user.settings.accepted_email_settings()):
-    #     return
-
-    # if notification.email_sent:
-    #     return
-        
-    # username = 'bounty hunter'
-    # if user and user.name:
-    #     username = user.name
+    if notification.email_sent:
+        return
 
     if kwargs['notification_name'] not in Email.templates:
-        return # TODO: Still do regular notification without email
+        return  # TODO: Still do regular notification without email
 
     email = Email(**kwargs)
 
     # TODO: Remove this - only use while debugging
     email.render_to_file()
 
-    # email_html = email.render()
+    email_html = email.render()
 
-    # email_txt = 'Hello {}! \n {}'.format(
-    #     username, string_data_email or string_data, )
-
-    # send_email(user.email, subject, email_txt, email_html)
-    # notification.email_sent = True
-    # notification.save()
+    send_email(user.email, subject, email_txt, email_html)
+    notification.email_sent = True
+    notification.save()

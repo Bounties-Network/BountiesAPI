88388033c5f2fc71cdda8d64f2741bd44dd8371b
Michael Misiewicz <mmisiewicz@gmail.com>
2018-09-15 16:50:54 -0400

    Cleanup code with pylint

--- bounties_api/analytics/pricing/pricepredictor.py
+++ bounties_api/analytics/pricing/pricepredictor.py
@@ -12,6 +12,7 @@ from gensim.models import Word2Vec
 from datetime import datetime
 import pickle
 
+
 class PricePredictor:
     """
     this class implments a simple interface to predict a Bounty price
@@ -19,41 +20,43 @@ class PricePredictor:
     issue type and platform.
     """
     # regexes for text cleaning
-    nonwords = re.compile('[^a-zA-Z \n]')
-    double_space = re.compile('\ {2,}')
-    double_newline = re.compile("\n{2,}")
-    urls = re.compile("https?://[0-9a-zA-Z\-\.\/\?\&\=]+")
+    nonwords = re.compile(r'[^a-zA-Z \n]')
+    double_space = re.compile(r'\ {2,}')
+    double_newline = re.compile(r'\n{2,}')
+    urls = re.compile(r'https?://[0-9a-zA-Z\-\.\/\?\&\=]+')
 
     # categorical variables definitions for one-hot encoding
     platforms = ['colorado', 'hiring', 'bounties-network', 'gitcoin']
     bounty_types = ['Code Review', None, 'Documentation', 'Bug',
-        'Improvement', 'Feature', 'Other', 'Security', 'Andere']
-    experience_levels = ['Beginner', None, 'Intermediate', 'Mittlere', 'Advanced']
+                    'Improvement', 'Feature', 'Other', 'Security', 'Andere']
+    experience_levels = ['Beginner', None,
+                         'Intermediate', 'Mittlere', 'Advanced']
     tokens = ['LOVE', 'GIFT', 'GEN', 'WYV', 'DAI', 'ADT', 'AION', 'ETH',
-    'COLO', 'ANT', 'BNFT', 'CLN', 'ZRX', 'KIWI', 'LPT', 'TRX', 'MANA', 'AVO']
-
+              'COLO', 'ANT', 'BNFT', 'CLN', 'ZRX', 'KIWI', 'LPT', 'TRX', 'MANA', 'AVO']
 
     def __init__(self):
         self.categories_w2vmodel = Word2Vec.load("data/categories_w2v.pkl")
         self.titles_w2vmodel = Word2Vec.load("data/titles_w2v.pkl")
         self.description_w2vmodel = Word2Vec.load("data/description_w2v.pkl")
-        self.description_d2vmodel = doc2vec.load("data/description_d2v.pkl")
+        self.description_d2vmodel = Doc2Vec.load("data/description_d2v.pkl")
         self.prediction_model = pickle.load(open("dats/elasticnet_model.pkl"))
         # or:
         # self.prediction_model = pickle.load(open("dats/xgboost_model.pkl"))
         nltk.download('stopwords')
         return
 
-    def clean_text(text):
-        urls_removed = urls.sub('', row).lower()
-        non_markdown = nonwords.sub(' ', urls_removed)
-        single_space = double_space.sub(' ', non_markdown)
-        single_newline = double_newline.sub('\n', single_space)
+    def clean_text(self, text):
+        """ Strip URLs, extra space, non-alnum and stopwords from a text """
+        urls_removed = self.urls.sub('', text).lower()
+        non_markdown = self.nonwords.sub(' ', urls_removed)
+        single_space = self.double_space.sub(' ', non_markdown)
+        single_newline = self.double_newline.sub('\n', single_space)
         description_list = single_newline.split(" ")
-        filtered_words = [word for word in description_list if word not in stopwords.words('english')]
+        filtered_words = [
+            word for word in description_list if word not in stopwords.words('english')]
         return " ".join(filtered_words)
 
-    def tag_doc(text, platform):
+    def tag_doc(self, text, platform):
         return TaggedDocument(words=simple_preprocess(text), tags=[platform])
 
     # Copied from Notebook for future work on retraining models
@@ -77,106 +80,125 @@ class PricePredictor:
     #     return (categories_w2vmodel, titles_w2vmodel, description_w2vmodel)
 
     # this function averages words by by looking them up in a w2v model
-    def average_categories_w2v_array(words):
+    def average_categories_w2v_array(self, words):
+        """ Determine the average vector for a list of categories """
         try:
-            running_total = categories_w2vmodel.wv[words[0]]
+            running_total = self.categories_w2vmodel.wv[words[0]]
         except (KeyError, IndexError):
             return np.zeros((100,))
         for w in words[1:]:
             try:
-                this_w = categories_w2vmodel.wv[w]
+                this_w = self.categories_w2vmodel.wv[w]
             except KeyError:
                 continue
-            running_total = (running_total + this_w)/2
+            running_total = (running_total + this_w) / 2
         return running_total
 
-    def average_titles_w2v_array(words):
+    def average_titles_w2v_array(self, words):
+        """ Determine the average vector for a title """
         s = words.split()
         try:
-            running_total = titles_w2vmodel.wv[s[0]]
+            running_total = self.titles_w2vmodel.wv[s[0]]
         except (KeyError, IndexError):
             return np.zeros((350,))
         for w in s[1:]:
             try:
-                this_w = titles_w2vmodel.wv[w]
+                this_w = self.titles_w2vmodel.wv[w]
             except KeyError:
                 continue
-            running_total = (running_total + this_w)/2
+            running_total = (running_total + this_w) / 2
         return running_total
 
-    def average_description_w2v_array(words):
+    def average_description_w2v_array(self, words):
+        """ Determine the average vector for a description """
         s = words.split()
         try:
-            running_total = description_w2vmodel.wv[s[0]]
+            running_total = self.description_w2vmodel.wv[s[0]]
         except (KeyError, IndexError):
             return np.zeros((350,))
         for w in s[1:]:
             try:
-                this_w = description_w2vmodel.wv[w]
+                this_w = self.description_w2vmodel.wv[w]
             except KeyError:
                 continue
-            running_total = (running_total + this_w)/2
+            running_total = (running_total + this_w) / 2
         return running_total
 
-    def get_one_hot(targets, nb_classes):
+    def get_one_hot(self, targets, nb_classes):
+        """ One hot encode categorical variables """
         res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
-        return res.reshape(list(targets.shape)+[nb_classes])
+        return res.reshape(list(targets.shape) + [nb_classes])
 
-    def get_days_to_deadline(deadline):
+    def get_days_to_deadline(self, deadline):
+        """ Take a timestamp and convert it to days from now, then bin it """
         # implement some cleanup logic in time windows
         # negative deadlines are mistakes due to API changes
         days_to_deadline = (deadline - datetime.now()).days
         if days_to_deadline < 0:
             return 0
         # assume that anything > 5y is infinte
-        if days_to_deadline > 5*365:
+        if days_to_deadline > 5 * 365:
             return 0
         return days_to_deadline
 
     def get_doc2_vec_array(self, platform, description_clean):
-        tagged_doc = tag_doc(description_clean, platform)
-        targets, regressors = (platform, self.description_d2vmodel.infer_vector(tagged_doc.words, steps=20))
+        """ Get the doc2vec array for a new text from
+        from the saved description d2v model """
+        tagged_doc = self.tag_doc(description_clean, platform)
+        targets, regressors = (
+            platform, self.description_d2vmodel.infer_vector(tagged_doc.words, steps=20))
         return (targets, regressors)
 
-    def generate_feature_array(self, title, description, categories, deadline_timestamp, experience_level,
-                              difficulty, token_type, platform):
+    def generate_feature_array(self, title, description, categories, experience_level, deadline_timestamp,
+                               token_type, bounty_type, platform):
+        """ Returns a feature vector for input into a sklearn predictor """
         # munge timestamp
-        days_to_deadline = get_days_to_deadline(deadline_timestamp)
+        days_to_deadline = self.get_days_to_deadline(deadline_timestamp)
         # apply text cleaning
-        description_clean = clean_text(description)
+        description_clean = self.clean_text(description)
         description_length = len(description.split())
-        title_clean = clean_text(title)
+        title_clean = self.clean_text(title)
         data_categories_clean = [x.strip().lower() for x in categories]
 
         # first, doc2vec of description
-        new_row = get_doc2_vec_array(platform, description_clean) # doc2vec of desc.
-        new_row = np.concatenate((new_row, np.array([days_to_deadline, description_length])), axis = None)
+        # doc2vec of desc.
+        new_row = self.get_doc2_vec_array(platform, description_clean)
+        new_row = np.concatenate(
+            (new_row, np.array([days_to_deadline, description_length])), axis=None)
 
         # categorical features (one-hot encoded)
-        el_one_hot = get_one_hot(np.array(self.experience_levels.index(experience_level)), len(self.experience_levels))
-        bt_one_hot = get_one_hot(np.array(self.bounty_types.index(bounty_type)), len(self.bounty_types))
-        pl_one_hot = get_one_hot(np.array(self.platforms.index(platform)), len(self.platforms))
-        ts_one_hot = get_one_hot(np.array(self.tokens.index(token_type), len(self.tokens))
-        new_row = np.concatenate((new_row, el_one_hot, bt_one_hot, pl_one_hot, ts_one_hot), axis = None)
+        el_one_hot = self.get_one_hot(np.array(self.experience_levels.index(
+            experience_level)), len(self.experience_levels))
+        bt_one_hot = self.get_one_hot(
+            np.array(self.bounty_types.index(bounty_type)), len(self.bounty_types))
+        pl_one_hot = self.get_one_hot(
+            np.array(self.platforms.index(platform)), len(self.platforms))
+        ts_one_hot = self.get_one_hot(
+            np.array(self.tokens.index(token_type)), len(self.tokens))
+        new_row = np.concatenate(
+            (new_row, el_one_hot, bt_one_hot, pl_one_hot, ts_one_hot), axis=None)
 
         # w2v features
-        new_row = np.concatenate((new_row, average_categories_w2v_array(data_categories_clean)), axis = None)
-        new_row = np.concatenate((new_row, average_titles_w2v_array(title_clean)), axis = None)
-        new_row = np.concatenate((new_row, average_description_w2v_array(description_clean)), axis = None)
+        new_row = np.concatenate(
+            (new_row, self.average_categories_w2v_array(data_categories_clean)), axis=None)
+        new_row = np.concatenate(
+            (new_row, self.average_titles_w2v_array(title_clean)), axis=None)
+        new_row = np.concatenate(
+            (new_row, self.average_description_w2v_array(description_clean)), axis=None)
 
         # all done!
-        new_row.shape = (1,1138)
+        new_row.shape = (1, 1138)
         return new_row
 
-    def predict(self, title, description, categories, difficulty,
-                deadline, token_type, platform):
-        features = generate_feature_array(title, description, categories,
-            deadline_timestamp, experience_level, difficulty, token_type, platform)
+    def predict(self, title, description, categories, experience_level,
+                deadline, token_type, bounty_type, platform):
+        """ Predicts a closing price (in USD) for a bounty """
+        features = self.generate_feature_array(title, description, categories,
+                                               deadline, experience_level, bounty_type, token_type, platform)
         result = self.prediction_model.predict(features)
         if result < 0:
             return 0
-        else:
-            return result
+        return result
 
-    def retrain():
+    def retrain(self):
         raise NotImplementedError("Not implemented yet!")

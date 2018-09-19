911ddc045ae690d520b39ef1ec69be4346fb3fea
William Villanueva <wvillanu89@gmail.com>
2018-09-19 14:46:44 -0500

    Screenshot service working/functional. :)

---
+++ serverless/puppeteer-screenshots/src/index.js
@@ -0,0 +1,50 @@
+const setup = require('./configs/setup');
+
+exports.handler = async (event, context, callback) => {
+  // For keeping the browser launch
+  context.callbackWaitsForEmptyEventLoop = false;
+  const url = event.url;
+  const key = event.key;
+  const browser = await setup.getBrowser();
+  try {
+    const result = await exports.run(browser, url, key);
+    callback(null, result);
+  } catch (e) {
+    callback(e);
+  }
+};
+
+exports.run = async (browser, url, key) => {
+  const page = await browser.newPage();
+  await page.goto(url,
+   {waitUntil: ['domcontentloaded', 'networkidle0']}
+  );
+  await page.setViewport({width: 1464, height: 764});
+  await page.screenshot({
+    path: '/tmp/screenshot.png',
+    clip: {
+      x: 64,
+      y: 64,
+      width: 1400,
+      height: 700,
+    },
+  });
+
+  const aws = require('aws-sdk');
+  const s3 = new aws.S3({apiVersion: '2006-03-01'});
+  const fs = require('fs');
+  const screenshot = await new Promise((resolve, reject) => {
+    fs.readFile('/tmp/screenshot.png', (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+  await s3.putObject({
+    Bucket: 'assets.bounties.network',
+    Key: key,
+    Body: screenshot,
+    ACL: 'public-read',
+  }).promise();
+  await page.close();
+  return 'done';
+};

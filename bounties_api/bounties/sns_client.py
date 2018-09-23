c1e644150829ad412e5fdc6e71f78096947fd722
William Villanueva <wvillanu89@gmail.com>
2018-09-23 13:55:51 -0500

    sns publisher client.

---
+++ bounties_api/bounties/sns_client.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+import logging
+from django.conf import settings
+
+
+logger = logging.getLogger('django')
+
+
+AWS_REGION = 'us-east-1'
+client = boto3.client('sns', region_name=AWS_REGION)
+
+
+def sns_publish(receiver, message):
+    if settings.LOCAL:
+        return
+    if settings.ENVIRONMENT not in ['production', 'rinkstaging', 'rinkeby']:
+        return
+    try:
+        client.publish('arn:aws:sns:us-east-1:802922962628:' + receiver, Message=json.dumps(message))
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])

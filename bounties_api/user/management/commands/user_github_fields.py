72c059cadb769d1f68fe08a9b191c248bebf6fda
Matt Garnett <matt@mattgarnett.com>
2018-10-10 11:53:11 +0200

    update user_github_fields script to scrape large and small version of the user's profile image and upload it to our s3

--- bounties_api/user/management/commands/user_github_fields.py
+++ bounties_api/user/management/commands/user_github_fields.py
@@ -24,42 +24,53 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             users = User.objects.all().exclude(
-                Q(github=''), Q(large_profile_image_url='')
+                Q(github='') |
+                Q(profile_touched_manually=True)
+            ).filter(
+                large_profile_image_url=''
             )
 
             for user in users:
-                image_r = None
+                url = 'https://api.github.com/users/{}'.format(user.github)
+                response = requests.get(url, headers={'Authorization': 'token ' + settings.GITHUB_TOKEN})
 
-                if user.github and not user.profile_touched_manually and not user.large_profile_image_url:
-                    github_username = user.github
-                    if not github_username:
-                        continue
-                    url = 'https://api.github.com/users/{}'.format(github_username)
-                    r = requests.get(
-                        url, headers={
-                            'Authorization': 'token ' + settings.GITHUB_TOKEN})
+                if response.status_code != 200:
+                    continue
 
-                    if r.status_code == 200:
-                        github_data = r.json()
-                        github_image = github_data.get('avatar_url')
-                        image_r = requests.get(github_image)
+                github_data = response.json()
 
-                if image_r and image_r.status_code == 200:
+                large_image_url = github_data.get('avatar_url') + '&s=300'
+                small_image_url = github_data.get('avatar_url') + '&s=64'
+
+                large_image_response = requests.get(large_image_url)
+                small_image_response = requests.get(small_image_url)
+
+                if large_image_response.status_code == 200 and small_image_response.status_code == 200:
                     try:
                         nonce = int(random() * 1000)
                         bucket = 'assets.bounties.network'
-                        key = '{}/userimages/{}-{}.jpg'.format(
-                            settings.ENVIRONMENT, user.public_address, nonce)
+
+                        small_key = '{}/userimages/{}-sm-{}.png'.format(settings.ENVIRONMENT, user.public_address, nonce)
+                        large_key = '{}/userimages/{}-lg-{}.png'.format(settings.ENVIRONMENT, user.public_address, nonce)
 
                         client.put_object(
-                            Body=image_r.content,
-                            ContentType=image_r.headers['content-type'],
+                            Body=small_image_response.content,
+                            ContentType=small_image_response.headers['content-type'],
+                            CacheControl='max-age=31536000',
                             Bucket=bucket,
                             ACL='public-read',
-                            Key=key)
+                            Key=small_key)
 
-                        user.small_profile_image_url = 'https://{}/{}'.format(bucket, key)
-                        user.large_profile_image_url = 'https://{}/{}'.format(bucket, key)
+                        client.put_object(
+                            Body=large_image_response.content,
+                            ContentType=large_image_response.headers['content-type'],
+                            CacheControl='max-age=31536000',
+                            Bucket=bucket,
+                            ACL='public-read',
+                            Key=large_key)
+
+                        user.small_profile_image_url = 'https://{}/{}'.format(bucket, small_key)
+                        user.large_profile_image_url = 'https://{}/{}'.format(bucket, large_key)
 
                         user.save()
 

1734d31b9401c38060c765f1bd6f56920da46fb0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-10-15 20:25:08 +0100

    Update swagger

--- bounties_api/std_bounties/urls.py
+++ bounties_api/std_bounties/urls.py
@@ -15,7 +15,6 @@ router.register(r'^bounty/(?P<bounty_id>\d+)/comment', views.BountyComments, 'bo
 urlpatterns = [
     url(r'^leaderboard/issuer/$', views.LeaderboardIssuer.as_view()),
     url(r'^leaderboard/fulfiller/$', views.LeaderboardFulfiller.as_view()),
-    url(r'^fulfiller/(?P<address>\d+)', views.UserProfile.as_view()),
     url(r'^token/$', views.Tokens.as_view()),
     url(r'^bounty/(?P<bounty_id>\d+)/fulfillment/(?P<fulfillment_id>\d+)/review/$', views.SubmissionReviews.as_view()),
     url(r'^', include(router.urls)),

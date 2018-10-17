de856bf835d17c0ea444807a83ae2a0177671ef1
codeluggage <matias.forbord@gmail.com>
2018-10-17 08:19:57 +0200

    Pep8

--- bounties_api/bounties/urls.py
+++ bounties_api/bounties/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.views.generic.base import TemplateView
 from django.conf.urls import url, include
 from django.contrib import admin
 from bounties.views import custom_sitemap_index
--- bounties_api/std_bounties/urls.py
+++ bounties_api/std_bounties/urls.py
@@ -25,16 +25,13 @@ router.register(r'^bounty', views.BountyViewSet)
 router.register(r'^fulfillment', views.FulfillmentViewSet)
 router.register(r'^category', views.CategoryViewSet)
 router.register(r'^reviews', views.ReviewsViewSet, 'user_reviews')
-router.register(r'^bounty/(?P<bounty_id>\d+)/comment',
-    views.BountyComments, 'bounty_comments')
+router.register(r'^bounty/(?P<bounty_id>\d+)/comment', views.BountyComments, 'bounty_comments')
 
 urlpatterns = [
     url(r'^leaderboard/issuer/$', views.LeaderboardIssuer.as_view()),
     url(r'^leaderboard/fulfiller/$', views.LeaderboardFulfiller.as_view()),
     url(r'^token/$', views.Tokens.as_view()),
-    url(r'^bounty/(?P<bounty_id>\d+)/fulfillment'
-        '/(?P<fulfillment_id>\d+)/review/$',
-        views.SubmissionReviews.as_view()),
+    url(r'^bounty/(?P<bounty_id>\d+)/fulfillment/(?P<fulfillment_id>\d+)/review/$', views.SubmissionReviews.as_view()),
     url(r'^$', swagger_view),
     url(r'^swagger.json$', open_api_view),
     url(r'^', include(router.urls)),

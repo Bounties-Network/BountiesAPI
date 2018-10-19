3eb898379d9c83e913a035319ac93c0bceb938ab
codeluggage <matias.forbord@gmail.com>
2018-10-20 01:59:22 +0200

    Revert "Remove old swagger references"
    This reverts commit 2748e1ef679284164d00e92502d136455ca579e6.

--- bounties_api/bounties/urls.py
+++ bounties_api/bounties/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     url(r'^auth/', include('user.auth_urls', namespace='auth')),
     url(r'^user/', include('user.user_urls', namespace='user')),
     url(r'^notification/', include('notifications.urls', namespace='notification')),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="docs"),
     url(r'^analytics/', include('analytics.urls', namespace='analytics')),
     url(r'^', include('std_bounties.urls', namespace='std_bounties')),
+    url(r'^swagger', schema_view, name="docs")
 ]

93c57c5abb31c3ecf131f9df28649b38a1ad18ac
William Villanueva <wvillanu89@gmail.com>
2018-12-21 13:12:03 -0700

    Logging service, yaml, dockerfile and kube config setup

---
+++ logging/Dockerfile
@@ -0,0 +1,5 @@
+FROM mikesir87/aws-cli
+
+USER root
+WORKDIR /home/logging
+COPY ./local.sh /logging/local.sh
---
+++ logging/local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+i=0
+while :
+do
+    if ls /var/log/containers/*production_bountiesapi*.log 1> /dev/null 2>&1; then
+        for f in /var/log/containers/*production_bountiesapi*.log; do aws s3 cp $f "s3://bountiesapilog/$(date +%Y/%m/%d)/$i-$(basename $f)"; :> $f; echo "$f"; done;
+        i=$((i+1));
+    fi
+    sleep 3600;
+done

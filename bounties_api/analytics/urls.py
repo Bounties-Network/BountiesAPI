5a3568e78fbc6820bf22dc9e0bcbb1cef71953ba
Matt Garnett <matt@mattgarnett.com>
2019-01-07 15:21:18 -0600

    add analytics endpoint to be used to determine a page view

--- bounties_api/analytics/urls.py
+++ bounties_api/analytics/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import TimelineBounties
+from .views import TimelineBounties, RecordPageView
 
 urlpatterns = [
+    url(r'^recordPageView/$', RecordPageView.as_view()),
     url(r'^', TimelineBounties.as_view())
 ]

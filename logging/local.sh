eadc37c840044da439056989c60b4be5336f961c
Matt Garnett <matt@mattgarnett.com>
2019-01-07 16:46:19 -0600

    add hour directory for logs

--- logging/local.sh
+++ logging/local.sh
@@ -4,7 +4,7 @@ i=0
 while :
 do
     if ls /var/log/containers/*production_bountiesapi*.log 1> /dev/null 2>&1; then
-        for f in /var/log/containers/*production_bountiesapi*.log; do aws s3 cp $f "s3://bountiesapilog/$(date +%Y/%m/%d)/$i-$(basename $f)"; :> $f; echo "$f"; done;
+        for f in /var/log/containers/*production_bountiesapi*.log; do aws s3 cp $f "s3://bountiesapilog/$(date +%Y/%m/%d/%H)/$i-$(basename $f)"; :> $f; echo "$f"; done;
         i=$((i+1));
     fi
     sleep 3600;

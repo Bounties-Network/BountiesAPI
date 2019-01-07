99ae1f1d289bada490b94a43167efb1c69dcd8a1
Matt Garnett <matt@mattgarnett.com>
2019-01-07 17:53:33 -0600

    change pageview endpoint

--- bounties_api/analytics/urls.py
+++ bounties_api/analytics/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import TimelineBounties, RecordPageView
 
 urlpatterns = [
-    url(r'^recordPageView/$', RecordPageView.as_view()),
+    url(r'^ping/$', RecordPageView.as_view()),
     url(r'^', TimelineBounties.as_view())
 ]

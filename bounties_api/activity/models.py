2011ff02a3beedfd839c75f8f59083ba65d11138
Matt Garnett <matt@mattgarnett.com>
2019-01-24 13:49:10 -0600

    initial activity model

---
+++ bounties_api/activity/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from activity.constants import ACTIVITY_TYPES
+from user.models import User
+from std_bounties.models import (
+    Bounty,
+    DraftBounty,
+    Fulfillment
+)
+
+
+class Target(models.Model):
+    bounty = models.OneToOneField(Bounty, null=True, blank=True, on_delete=models.CASCADE)
+    draft = models.OneToOneField(DraftBounty, null=True, blank=True, on_delete=models.CASCADE)
+    fulfillment = models.OneToOneField(Fulfillment, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Activity(models.Model):
+    actor = models.ForeignKey('user.User', null=False)
+    verb = models.CharField(max_length=3, choices=ACTIVITY_TYPES)
+    date = models.DateTimeField(auto_now_add=True)
+    target = models.ForeignKey('activity.Target', null=True)

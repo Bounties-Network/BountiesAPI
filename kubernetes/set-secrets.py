5e0b161c59561600aedc64ef4f58d6d8fec17f76
Matt Garnett <matt@mattgarnett.com>
2019-03-07 16:00:40 -0600

    reorganize project for fora dev env:

---
+++ kubernetes/set-secrets.py
@@ -0,0 +1,54 @@
+import json
+import subprocess
+
+from base64 import b64decode
+
+
+namespace = 'fora-dev'
+
+SECRETS_MAP = {
+	'redis': ['host', 'port'],
+	'psql': ['host', 'port', 'user', 'password'],
+	'aws-credentials': ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY'],
+	'django': ['secret', 'deploy-url'],
+	'rollbar': ['token'],
+	'sqs': ['host'],
+	'eth': ['network'],
+	'slack': ['token'],
+        'environment': ['name'],
+        'sns': ['host'],
+}
+
+for secret_name in SECRETS_MAP:
+	out = subprocess.Popen(
+		['kubectl', 'get', 'secret', '-n', namespace, secret_name, '-o', 'json'],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.STDOUT
+	)
+
+	stdout, stderr = out.communicate()
+
+	try:
+		secret = json.loads(stdout)['data']
+	except:
+		secret = {}
+
+	print(secret_name)
+	for key in SECRETS_MAP[secret_name]:
+		current_value = b64decode(secret[key] if key in secret else '')
+		current_value = current_value.decode('utf-8')
+
+		display_value = f' ({current_value})' if current_value else ''
+		value = input(f'{key}{display_value}: ')
+
+		secret.update({key: value or current_value})
+
+	literals = []
+	for key, value in secret.items():
+		literals.append(f'--from-literal={key}=\'{value}\'')
+
+	args = ['kubectl', 'create', 'secret', '-n', namespace, 'generic', secret_name, *literals, '--dry-run', '-o', 'yaml', '|', 'kubectl', 'apply', '-f', '-']
+
+	print('')
+	print(' '.join(args))
+	print('')

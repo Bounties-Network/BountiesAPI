81dbcffa0ea585031ba47dc69f027123a28edc5b
Matt Garnett <matt@mattgarnett.com>
2019-03-08 14:30:07 -0600

    fix linting

--- kubernetes/set-secrets.py
+++ kubernetes/set-secrets.py
@@ -7,48 +7,42 @@ from base64 import b64decode
 namespace = 'fora-dev'
 
 SECRETS_MAP = {
-	'redis': ['host', 'port'],
-	'psql': ['host', 'port', 'user', 'password'],
-	'aws-credentials': ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY'],
-	'django': ['secret', 'deploy-url'],
-	'rollbar': ['token'],
-	'sqs': ['host'],
-	'eth': ['network'],
-	'slack': ['token'],
-        'environment': ['name'],
-        'sns': ['host'],
+    'redis': ['host', 'port'],
+    'psql': ['host', 'port', 'user', 'password'],
+    'aws-credentials': ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY'],
+    'django': ['secret', 'deploy-url'],
+    'rollbar': ['token'],
+    'sqs': ['host'],
+    'eth': ['network'],
+    'slack': ['token'],
+    'environment': ['name'],
+    'sns': ['host'],
 }
 
 for secret_name in SECRETS_MAP:
-	out = subprocess.Popen(
-		['kubectl', 'get', 'secret', '-n', namespace, secret_name, '-o', 'json'],
-		stdout=subprocess.PIPE,
-		stderr=subprocess.STDOUT
-	)
+    out = subprocess.Popen(
+        ['kubectl', 'get', 'secret', '-n', namespace, secret_name, '-o', 'json'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
 
-	stdout, stderr = out.communicate()
+    stdout, stderr = out.communicate()
 
-	try:
-		secret = json.loads(stdout)['data']
-	except:
-		secret = {}
+    try:
+        secret = json.loads(stdout)['data']
+    except:  # noqa
+        secret = {}
 
-	print(secret_name)
-	for key in SECRETS_MAP[secret_name]:
-		current_value = b64decode(secret[key] if key in secret else '')
-		current_value = current_value.decode('utf-8')
+    print(secret_name)
+    for key in SECRETS_MAP[secret_name]:
+        current_value = b64decode(secret[key] if key in secret else '')
+        current_value = current_value.decode('utf-8')
 
-		display_value = f' ({current_value})' if current_value else ''
-		value = input(f'{key}{display_value}: ')
+        display_value = f' ({current_value})' if current_value else ''
+        value = input(f'{key}{display_value}: ')
 
-		secret.update({key: value or current_value})
+        secret.update({key: value or current_value})
 
-	literals = []
-	for key, value in secret.items():
-		literals.append(f'--from-literal={key}=\'{value}\'')
-
-	args = ['kubectl', 'create', 'secret', '-n', namespace, 'generic', secret_name, *literals, '--dry-run', '-o', 'yaml', '|', 'kubectl', 'apply', '-f', '-']
-
-	print('')
-	print(' '.join(args))
-	print('')
+    literals = []
+    for key, value in secret.items():
+        literals.append(f'--from-literal={key}=\'{value}\'')

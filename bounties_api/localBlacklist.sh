493f4ad1d5c8c66dd129b5492a71aa6fc789fee5
mbeylin <mark_beylin@hotmail.com>
2019-05-15 22:03:36 -0400

    adds blacklist to local deployment

---
+++ bounties_api/localBlacklist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# In production, this is an every 5 minute cronjob. Locally, we just run it regularly using a bash script
+while :
+do
+	sleep 30
+	python3 manage.py bounties_subscriber --blacklist
+	sleep 300
+done

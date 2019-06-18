c860c8c83a85e7e548f828794a0ed7726c6c9bfe
mbeylin <mark_beylin@hotmail.com>
2019-06-18 18:39:32 -0400

    removes print statements

--- bounties_api/notifications/notification_client.py
+++ bounties_api/notifications/notification_client.py
@@ -269,12 +269,7 @@ class NotificationClient:
 
         users = [user for user in applicants if user not in fulfillers]
 
-        print('length of fulfillers:')
-        print(len(fulfillers))
-        print('length of applicants:')
-        print(len(users))
         for user in set(users):
-            print('about to send applicant email')
             create_bounty_notification(
                 bounty=bounty,
                 uid='{}-{}-notification'.format(kwargs.get('uid'), user.public_address),
@@ -287,7 +282,6 @@ class NotificationClient:
                 is_activity=False
             )
         for user in set(fulfillers):
-            print('about to send fulfiller email')
             create_bounty_notification(
                 bounty=bounty,
                 uid='{}-{}-notification'.format(kwargs.get('uid'), user.public_address),

b1e4a1d70435ab06ca64f7ef29aa3c648cd5cfee
mbeylin <mark_beylin@hotmail.com>
2019-06-18 15:46:29 -0400

    testing fix for notifications settings getter

--- bounties_api/user/models.py
+++ bounties_api/user/models.py
@@ -50,7 +50,7 @@ class Settings(models.Model):
             **self.emails['both'],
             **self.emails['fulfiller']}
 
-        opted_out = [setting for setting in merged_settings if not merged_settings[setting]]
+        opted_out = [setting for setting in merged_settings if merged_settings[setting] == False]
         print('opted out')
         print(opted_out)
         return [setting for setting in list(notifications.keys) if setting not in opted_out]

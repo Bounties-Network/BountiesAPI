c09a17690ab12bf1830995df3ff43dbef905bfaf
mbeylin <mark_beylin@hotmail.com>
2019-06-27 14:28:28 -0400

    WIP of adding token param to leaderboard queries

--- bounties_api/std_bounties/views/fulfiller_leaderboard_views.py
+++ bounties_api/std_bounties/views/fulfiller_leaderboard_views.py
@@ -11,6 +11,7 @@ class LeaderboardFulfiller(APIView):
     def get(request):
         sql_param = ''
         platform_in = extractInParams(request, 'platform', 'platform__in')
+        token_in = extractInParams(request, 'token', 'token__in')
         startIndex, endIndex = limitOffsetParams(request)
         if platform_in:
             sql_param = 'AND ( '
@@ -21,7 +22,11 @@ class LeaderboardFulfiller(APIView):
                                            len(platform_in), '=')
             sql_param += ' )'
         platform_in = platform_in + platform_in
-
+        if token_in:
+            sql_param += 'AND ( '
+            sql_param += 'bounty.token_contract = '
+            sql_param += token_in[0]
+            sql_param += ' )'
         formatted_query = LEADERBOARD_FULFILLER_QUERY.format(sql_param)
         cursor = connection.cursor()
         cursor.execute(formatted_query, platform_in)

f89b8c39ab518113de920c284edd3ec5afa3f88d
mbeylin <mark_beylin@hotmail.com>
2019-07-08 15:20:56 -0400

    updates url

--- bounties_api/std_bounties/urls.py
+++ bounties_api/std_bounties/urls.py
@@ -10,14 +10,14 @@ router.register(r'^fulfillment', views.FulfillmentViewSet)
 router.register(r'^category', views.CategoryViewSet)
 router.register(r'^reviews', views.ReviewsViewSet, 'user_reviews')
 router.register(r'^bounty/(?P<bounty_id>\d+)/comment', views.BountyComments, 'bounty_comments')
-router.register(r'^bounty/(?P<bounty_id>\d+)/fulfillment/(?P<fulfillment_id>\d+)/comment', views.FulfillmentComments, 'fulfillment_comments')
+router.register(r'^fulfillment/(?P<fulfillment_id>\d+)/comment', views.FulfillmentComments, 'fulfillment_comments')
 router.register(r'^bounty/(?P<bounty_id>\d+)/application', views.FulfillerApplicationViewSet, 'bounty_applications')
 
 urlpatterns = [
     url(r'^leaderboard/issuer/$', views.LeaderboardIssuer.as_view()),
     url(r'^leaderboard/fulfiller/$', views.LeaderboardFulfiller.as_view()),
     url(r'^token/$', views.Tokens.as_view()),
-    url(r'^fulfillment/(?P<fulfillment_id>\d+)/review/$', views.FulfillmentReviews.as_view()),
+    url(r'^bounty/(?P<bounty_id>\d+)/fulfillment/(?P<fulfillment_id>\d+)/review/$', views.FulfillmentReviews.as_view()),
     url(r'^application/(?P<pk>\d+)/$', views.FulfillerApplicationApplicantView.as_view()),
     url(r'^', include(router.urls))
 ]

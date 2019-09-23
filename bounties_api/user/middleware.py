6ca3ff90a9c772ca38efa284b55627913c15b321
Eric Juta <ericjohn.juta@consensys.net>
2019-09-23 12:36:18 +0100

    Add to middleware graphql api auth cookie

--- bounties_api/user/middleware.py
+++ bounties_api/user/middleware.py
@@ -35,7 +35,25 @@ class AuthenticationMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         uuid_cookie = request.COOKIES.get('uuid')
         user_id_cookie = request.COOKIES.get('user_id')
+        graphql_authorization_cookie = request.COOKIES.get('authorization')
 
+        if not graphql_authorization_cookie:
+            expires = datetime.datetime.strftime(
+                datetime.datetime.utcnow() +
+                datetime.timedelta(
+                    seconds=max_age),
+                "%a, %d-%b-%Y %H:%M:%S GMT")
+            response.set_cookie(
+                'authorization',
+                value=jwt.encode({
+                    'https://hasura.io/jwt/claims': {
+                        'x-hasura-allowed-roles': ['user'],
+                        'x-hasura-default-role': 'user',
+                        'x-hasura-user-id': request.current_user.id
+                    },
+                    'exp': expires}, settings.SECRET_KEY, algorithm="HS256"),
+                secure=False, httponly=True, expires=expires
+            )
         if not uuid_cookie:
             expires = datetime.datetime.strftime(
                 datetime.datetime.utcnow() +

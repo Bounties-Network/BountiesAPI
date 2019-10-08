336a88503e50feaa4392eb87a083d72648e27fca
Eric Juta <ericjohn.juta@consensys.net>
2019-10-09 00:58:05 +0100

    Add Fluentd config and add winston + fluentd logger to contract_subscriber + build + push to dockerhub

--- contract_subscriber/index.js
+++ contract_subscriber/index.js
@@ -1,58 +1,71 @@
-'use strict';
+"use strict";
 
-const delay = require('delay');
-const rollbar = require('./rollbar');
-const { StandardBounties, getBlock } = require('./web3_config');
-const { getAsync, writeAsync } = require('./redis_config');
-const { sendEvents } = require('./eventsRetriever');
-const { CONTRACT_VERSION } = require('./constants');
+const delay = require("delay");
+const rollbar = require("./rollbar");
+const fluentd = require("./fluentd");
+const { StandardBounties, getBlock } = require("./web3_config");
+const { getAsync, writeAsync } = require("./redis_config");
+const { sendEvents } = require("./eventsRetriever");
+const { CONTRACT_VERSION } = require("./constants");
 
 async function handler() {
-	while (true) {
-		try {
-			// I use past events vs. subscribe in order to preserve ordering - FIFO
-			// Also, subscribe is just polling - the socket connection does not provide the additional behavior, so these
-			// are essentially accomplishing the same thing
+  while (true) {
+    try {
+      // I use past events vs. subscribe in order to preserve ordering - FIFO
+      // Also, subscribe is just polling - the socket connection does not provide the additional behavior, so these
+      // are essentially accomplishing the same thing
 
-			// StandardBounties latest events
-			let fromBlock = await getAsync(`currentBlock_${CONTRACT_VERSION}`) || 0;
-			fromBlock = parseInt(fromBlock);
-			const latestBlockData = await getBlock('latest');
-			const latestBlock = latestBlockData.number;
-			console.log('fromBlock: ', fromBlock);
-			console.log('latestBlock: ', latestBlock)
-			let eventBlock;
+      // StandardBounties latest events
+      let fromBlock = (await getAsync(`currentBlock_${CONTRACT_VERSION}`)) || 0;
+      fromBlock = parseInt(fromBlock);
+      const latestBlockData = await getBlock("latest");
+      const latestBlock = latestBlockData.number;
+      // console.log('fromBlock: ', fromBlock);
+      // console.log('latestBlock: ', latestBlock)
+      let eventBlock;
 
-			while (fromBlock < latestBlock) {
-				let events = await StandardBounties.getPastEvents({fromBlock, toBlock: fromBlock + 100000});
-				console.log('currentCheck: ', fromBlock);
-				eventBlock = await sendEvents(events);
-				if (eventBlock) {
-					break;
-				}
-				fromBlock += 100000;
-			}
+      while (fromBlock < latestBlock) {
+        let events = await StandardBounties.getPastEvents({ fromBlock, toBlock: fromBlock + 100000 });
+        fluentd.log({
+          level: "info",
+          message: JSON.stringify({ fromBlock, currentBlock, CONTRACT_VERSION })
+        });
+        eventBlock = await sendEvents(events);
+        if (eventBlock) {
+          fluentd.log({
+            level: "info",
+            message: JSON.stringify({ eventBlock, fromBlock, currentBlock, CONTRACT_VERSION })
+          });
+          break;
+        }
+        fromBlock += 100000;
+      }
 
-			console.log('eventBlock: ', eventBlock);
-			if (eventBlock) {
-				await writeAsync(`currentBlock_${CONTRACT_VERSION}`, eventBlock);
-			}
+      // console.log('eventBlock: ', eventBlock);
+      if (eventBlock) {
+        await writeAsync(`currentBlock_${CONTRACT_VERSION}`, eventBlock);
+      }
 
-			await delay(1000);
-
-		} catch (err) {
-			rollbar.error(err);
-			console.log(err);
-			// ignore constant RPC response error from Infura temporarily
-			if (err.message !== 'Invalid JSON RPC response: ""') {
-				// exit with error so kubernettes will automatically restart the job
-				process.exit(1);
-			} else {
-				// try again in a little while
-				await delay(5000);
-			}
-		}
-	}
+      await delay(1000);
+    } catch (err) {
+      rollbar.error(err);
+      fluentd.log({
+        level: "info",
+        message: JSON.stringify({ eventBlock, fromBlock, currentBlock, CONTRACT_VERSION })
+      });
+      fluentd.error(err);
+      // console.log(err);
+      // ignore constant RPC response error from Infura temporarily
+      if (err.message !== 'Invalid JSON RPC response: ""') {
+        //  exit with error so kubernettes will automatically restart the job
+        fluentd.warn(err);
+        process.exit(1);
+      } else {
+        // try again in a little while
+        await delay(5000);
+      }
+    }
+  }
 }
 
 handler();

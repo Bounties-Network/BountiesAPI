4e5d6c627fe90897394f4bccdfb8f8e0fa01694d
Eric Juta <ericjohn.juta@consensys.net>
2019-10-09 01:17:34 +0100

    Hotfix fluentd setup

--- contract_subscriber/fluentd.js
+++ contract_subscriber/fluentd.js
@@ -4,10 +4,11 @@ const { combine, timestamp, label, prettyPrint } = format;
 var config = {
   host: "localhost",
   port: 24224,
-  timeout: 3.0
+  timeout: 3.0,
+  requireAckResponse: true
 };
 var fluentTransport = require("fluent-logger").support.winstonTransport();
-var logger = new winston.Logger({
+var logger = winston.createLogger({
   format: combine(label({ label: "contract_subscriber" }), timestamp(), prettyPrint()),
   transports: [
     new fluentTransport("contract_subscriber", config),

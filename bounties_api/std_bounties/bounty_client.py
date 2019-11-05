c30861486ad5b50f7692cf90cd8bf43bcfef6226
mbeylin <mark_beylin@hotmail.com>
2019-11-05 17:52:03 -0500

    removes print statements

--- bounties_api/std_bounties/bounty_client.py
+++ bounties_api/std_bounties/bounty_client.py
@@ -306,8 +306,6 @@ class BountyClient:
             saved_bounty.token_decimals,
             saved_bounty.fulfillment_amount
         )[0]
-        print(saved_bounty.fulfillment_amount)
-        print(saved_bounty.usd_price)
         saved_bounty.save()
 
         return saved_bounty

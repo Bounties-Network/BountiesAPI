ffd479aa916443cbe3da800c5f56843305d56e36
mbeylin <mark_beylin@hotmail.com>
2019-11-11 18:27:41 -0500

    finishes bounty views implementation

--- bounties_api/std_bounties/views/view_views.py
+++ bounties_api/std_bounties/views/view_views.py
@@ -11,12 +11,8 @@ notification_client = NotificationClient()
 class BountyViews(APIView):
     @staticmethod
     def post(request, bounty_id):
-        print('viewing bounty')
-        print(bounty_id)
         bounty = get_object_or_404(Bounty, id=bounty_id)
-        print('got bounty')
         current_user = request.current_user
-        print('got user')
         serializer = ViewSerializer(
             data={
                 **request.data,
@@ -24,14 +20,9 @@ class BountyViews(APIView):
                 'bounty': bounty.pk
             }
         )
-        print('1')
         serializer.is_valid(raise_exception=True)
-        print('2')
         serializer.save()
-        print('3')
         bounty.view_count += 1
-        print('4')
         bounty.save()
-        print('5')
 
         return JsonResponse(data=serializer.data)

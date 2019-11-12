6ec95425ddff543a442535bed13efd1e9e5077fb
mbeylin <mark_beylin@hotmail.com>
2019-11-12 16:36:03 -0500

    adds script for updating viewcount

---
+++ bounties_api/std_bounties/management/commands/update_views.py
@@ -0,0 +1,26 @@
+import requests
+from math import pow
+from decimal import Decimal
+from django.core.management.base import BaseCommand
+from std_bounties.models import View, Bounty, Fulfillment
+import logging
+import sys
+
+logger = logging.getLogger('django')
+
+
+class Command(BaseCommand):
+    help = 'Update the views counts on bounties'
+
+    def handle(self, *args, **options):
+        try:
+            all_bounties = Bounty.objects;
+            for bounty in all_bounties:
+                fulfillment_count = Fulfillment.objects.filter(bounty_id=bounty.id).count();
+                if (bounty.view_count < fulfillment_count):
+                    bounty.view_count += fulfillment_count;
+
+        except Exception as e:
+            # goes to rollbar
+            logger.exception(e)
+            sys.exit(1)

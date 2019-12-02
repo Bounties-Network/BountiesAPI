d32eaabf668b388e44502e5e70efe31705fd2034
mbeylin <mark_beylin@hotmail.com>
2019-12-02 14:22:35 -0500

    fixes syntax

--- bounties_api/std_bounties/management/commands/populate_activities_comments.py
+++ bounties_api/std_bounties/management/commands/populate_activities_comments.py
@@ -2,7 +2,7 @@ import requests
 from math import pow
 from decimal import Decimal
 from django.core.management.base import BaseCommand
-from std_bounties.models import Activity, Community, Bounty, Event, Comment, FulfillerApplication
+from std_bounties.models import Activity, Community, Bounty, Event, Comment, Fulfillment, FulfillerApplication
 import logging
 import sys
 
@@ -16,12 +16,12 @@ class Command(BaseCommand):
         try:
             all_comments = Comment.objects.all()
             for comment in all_comments:
-                bounty = Bounty.objects.filter(comment = comment)
-                fulfillment = Fulfillment.objects.filter(comment = comment)
+                bounty = Bounty.objects.filter(comment=comment)
+                fulfillment = Fulfillment.objects.filter(comment=comment)
                 print('got bounty')
                 print(bounty)
                 print('got ful')
-                print(ful)
+                print(fulfillment)
 
         except Exception as e:
             # goes to rollbar
--- bounties_api/std_bounties/management/commands/populate_activities_events.py
+++ bounties_api/std_bounties/management/commands/populate_activities_events.py
@@ -2,7 +2,7 @@ import requests
 from math import pow
 from decimal import Decimal
 from django.core.management.base import BaseCommand
-from std_bounties.models import Activity, Community, Bounty, Event, Comment, FulfillerApplication
+from std_bounties.models import User, Activity, Community, Bounty, Event, Comment, FulfillerApplication
 import logging
 import sys
 
@@ -16,16 +16,16 @@ class Command(BaseCommand):
         try:
             all_events = Event.objects.all()
             for event in all_events:
-                user = User.objects.get(public_address = event.transaction_from.lower())
-                bounty = Bounty.objects.get(id = event.bounty_id)
+                user = User.objects.get(public_address=event.transaction_from.lower())
+                bounty = Bounty.objects.get(id=event.bounty_id)
                 activity = Activity.objects.create(
-                    event_type = event.event,
-                    bounty_id = event.bounty_id,
-                    fulfillment_id = event.fulfillment_id,
-                    date = event.event_date,
-                    transaction_hash = event.transaction_hash,
-                    user_id = user.id,
-                    community_id = bounty.community_id)
+                    event_type=event.event,
+                    bounty_id=event.bounty_id,
+                    fulfillment_id=event.fulfillment_id,
+                    date=event.event_date,
+                    transaction_hash=event.transaction_hash,
+                    user_id=user.id,
+                    community_id=bounty.community_id)
 
         except Exception as e:
             # goes to rollbar

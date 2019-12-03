9d113f6027a5fc0a1f3477938f8f689749e061b4
mbeylin <mark_beylin@hotmail.com>
2019-12-03 12:38:12 -0500

    updates activities table populating script

--- bounties_api/std_bounties/management/commands/populate_activities_comments.py
+++ bounties_api/std_bounties/management/commands/populate_activities_comments.py
@@ -16,17 +16,29 @@ class Command(BaseCommand):
         try:
             all_comments = Comment.objects.all()
             for comment in all_comments:
-                bounty = Bounty.objects.filter(comments__in=[comment]).first()
-                fulfillment = Fulfillment.objects.filter(comments__in=[comment]).first()
-                Activity.objects.create(
-                    event_type='Comment',
-                    bounty_id=bounty.id,
-                    fulfillment_id=fulfillment.id,
-                    comment_id=comment.id,
-                    date=comment.created,
-                    user_id=comment.user_id,
-                    community_id=comment.community_id)
+                try:
+                    bounty = Bounty.objects.get(comments__in=[comment])
+                    Activity.objects.create(
+                        event_type='Comment',
+                        bounty_id=bounty.id,
+                        comment_id=comment.id,
+                        date=comment.created,
+                        user_id=comment.user_id,
+                        community_id=comment.community_id)
+                except Bounty.DoesNotExist:
+                    pass
 
+                try:
+                    fulfillment = Fulfillment.objects.get(comments__in=[comment])
+                    Activity.objects.create(
+                        event_type='Comment',
+                        fulfillment_id=fulfillment.id,
+                        comment_id=comment.id,
+                        date=comment.created,
+                        user_id=comment.user_id,
+                        community_id=comment.community_id)
+                except Fulfillment.DoesNotExist:
+                    pass
         except Exception as e:
             # goes to rollbar
             logger.exception(e)

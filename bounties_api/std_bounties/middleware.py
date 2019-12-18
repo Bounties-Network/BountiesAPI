18dced71f94c0497fd006c044fe48247d23a68b0
Eric Juta <ericjohn.juta@consensys.net>
2019-12-18 16:03:20 +0000

    Got it working OMG, comment out logging

--- bounties_api/std_bounties/middleware.py
+++ bounties_api/std_bounties/middleware.py
@@ -3,9 +3,9 @@ import json
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 from bleach import clean
-import logging
+# import logging
 
-logger = logging.getLogger('django')
+# logger = logging.getLogger('django')
 
 
 class SanitizeDescriptionMiddleware(MiddlewareMixin):
@@ -13,12 +13,12 @@ class SanitizeDescriptionMiddleware(MiddlewareMixin):
         if "bounty" in request.path:
             content = json.loads(response.content.decode('utf-8'))
             description = content.get('description')
-            logging.info('description: %s', description)
+            # logging.info('description: %s', description)
             if description:
                 new_description = clean(description)
                 content['description'] = new_description
-                logging.info('new_description: %s', new_description)
-                logging.info('new_content: %s', content)
+                # logging.info('new_description: %s', new_description)
+                # logging.info('new_content: %s', content)
                 response.content = json.dumps(content)
 
         return response
